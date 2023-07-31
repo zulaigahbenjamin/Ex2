420d6eb965556fad5be608f4f7239f750829984c
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-07-31 15:24:46 +0200

    final

---
+++ Person.js
@@ -0,0 +1,11 @@
+// Person.js
+class Person {
+    constructor(firstName, lastName, emailAddress) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.emailAddress = emailAddress;
+    }
+  }
+  
+  module.exports = Person;
+  
---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const Person = require('./Person');
+
+// Create three different Person objects
+const person1 = new Person('Zulaigah', 'Benjamin', 'zulaigah@gmail.com');
+const person2 = new Person('Mariam', 'Noor', 'Mariam@gmail.com');
+const person3 = new Person('Aisha', 'Baker', 'Aishajohnson@gmail.com');
+
+// Display the information of each person
+console.log('Person 1:', person1);
+console.log('Person 2:', person2);
+console.log('Person 3:', person3);
